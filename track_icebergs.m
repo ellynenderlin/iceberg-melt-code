2fb7f7e74c365961c8b9430d358215c4cf7436a2
Ellyn Enderlin <ellynenderlin@boisestate.edu>
2023-02-10 15:56:39 -0700

    added interactive zooming to map icebergs

--- track_icebergs.m
+++ track_icebergs.m
@@ -26,61 +26,119 @@ cd_to_datefolder = ['cd ',dir_output,'/',DEM1.time,'-',DEM2.time,'/']; eval(cd_t
 
 %plot co-registered DEMs
 DEM1.z_elpsd_adjust(DEM1.z_elpsd_adjust<0) = 0; DEM2.z_elpsd_adjust(DEM2.z_elpsd_adjust<0) = 0; %remove elevations less than zero for plotting purposes
-figure1 = figure; set(figure1,'position',[0 600 900 600]);
-imagesc(DEM1.x,DEM1.y,DEM1.z_elpsd_adjust); axis xy equal; set(gca,'clim',[min(DEM1.z_elpsd_adjust(~isnan(DEM1.z_elpsd_adjust))) min(DEM1.z_elpsd_adjust(~isnan(DEM1.z_elpsd_adjust)))+40]); colormap(gca,'jet'); colorbar; grid on;
+figure1 = figure; set(figure1,'position',[0 600 800 600]);
+imagesc(DEM1.x,DEM1.y,DEM1.z_elpsd_adjust); axis xy equal; grid on;
+set(gca,'clim',[min(DEM1.z_elpsd_adjust(~isnan(DEM1.z_elpsd_adjust))) min(DEM1.z_elpsd_adjust(~isnan(DEM1.z_elpsd_adjust)))+40]); colormap(gca,'hot'); colorbar; 
 set(gca,'xtick',[min(DEM1.x):round(range(DEM1.x)/10000)*1000:max(DEM1.x)],'xticklabel',[min(DEM1.x)/1000:round(range(DEM1.x)/10000):max(DEM1.x)/1000],...
     'ytick',[min(DEM1.y):round(range(DEM1.y)/10000)*1000:max(DEM1.y)],'yticklabel',[min(DEM1.y)/1000:round(range(DEM1.y)/10000):max(DEM1.y)/1000]);
-figure2 = figure; set(figure2,'position',[975 600 900 600]);
-imagesc(DEM2.x,DEM2.y,DEM2.z_elpsd_adjust); axis xy equal; set(gca,'clim',[min(DEM2.z_elpsd_adjust(~isnan(DEM2.z_elpsd_adjust))) min(DEM2.z_elpsd_adjust(~isnan(DEM2.z_elpsd_adjust)))+40]); colormap(gca,'jet'); colorbar; grid on;
+figure2 = figure; set(figure2,'position',[975 600 800 600]);
+imagesc(DEM2.x,DEM2.y,DEM2.z_elpsd_adjust); axis xy equal; grid on;
+set(gca,'clim',[min(DEM2.z_elpsd_adjust(~isnan(DEM2.z_elpsd_adjust))) min(DEM2.z_elpsd_adjust(~isnan(DEM2.z_elpsd_adjust)))+40]); colormap(gca,'hot'); colorbar; 
 set(gca,'xtick',[min(DEM2.x):round(range(DEM2.x)/10000)*1000:max(DEM2.x)],'xticklabel',[min(DEM2.x)/1000:round(range(DEM2.x)/10000):max(DEM2.x)/1000],...
     'ytick',[min(DEM2.y):round(range(DEM2.y)/10000)*1000:max(DEM2.y)],'yticklabel',[min(DEM2.y)/1000:round(range(DEM2.y)/10000):max(DEM2.y)/1000]);
 
 %plot images
-figure3 = figure; set(figure3,'position',[0 0 900 600]); ax3=gca;
+figure3 = figure; set(figure3,'position',[0 0 800 600]); ax3=gca;
 imagesc(IM1.x,IM1.y,IM1.z); axis xy equal; colormap(gca,'gray'); grid on; set(gca,'clim',[0 200]); hold on;
 set(gca,'xtick',[min(DEM1.x):round(range(DEM1.x)/10000)*1000:max(DEM1.x)],'xticklabel',[min(DEM1.x)/1000:round(range(DEM1.x)/10000):max(DEM1.x)/1000],...
     'ytick',[min(DEM1.y):round(range(DEM1.y)/10000)*1000:max(DEM1.y)],'yticklabel',[min(DEM1.y)/1000:round(range(DEM1.y)/10000):max(DEM1.y)/1000]);
 
-figure4 = figure; set(figure4,'position',[975 0 900 600]); ax4=gca;
+figure4 = figure; set(figure4,'position',[975 0 800 600]); ax4=gca;
 imagesc(IM2.x,IM2.y,IM2.z); axis xy equal; colormap(gca,'gray'); grid on; set(gca,'clim',[0 200]); hold on;
 set(gca,'xtick',[min(DEM2.x):round(range(DEM2.x)/10000)*1000:max(DEM2.x)],'xticklabel',[min(DEM2.x)/1000:round(range(DEM2.x)/10000):max(DEM2.x)/1000],...
     'ytick',[min(DEM2.y):round(range(DEM2.y)/10000)*1000:max(DEM2.y)],'yticklabel',[min(DEM2.y)/1000:round(range(DEM2.y)/10000):max(DEM2.y)/1000]);
 
 
 % pick iceberg coordinates
-disp('Click on each iceberg you can visually ID on both DEMs. Max is 30!');
-disp('earlier DEM');
-% col = spring(30); % color scheme for plotting
-more=1; % select more icebergs when more=1
-i=0; % index for storing coordinates
-axes(ax3); % bring figure 3 axes to front
-while more==1
-    axes(ax3);
-    i=i+1;
-    [PSx_early(i),PSy_early(i)] = ginput(1);
-    plot(gca,PSx_early(i),PSy_early(i),'.','markersize',10,'color','y');
-    
-    str = input('Do you see more icebergs (y/n)?','s');
-    if strcmp(str,'n')
-        more=more+1;
-    end 
-end
-%add number labels
-axes(ax3);
-for i = 1:length(PSx_early)
-   text(PSx_early(i)+300,PSy_early(i),num2str(i),'color','y','fontsize',18); hold on;
-end
-disp('later DEM: Now, click on the same icebergs in the other image');
-disp('   You can click until the same number of icebergs have been selected');
-figure(figure4);
-for j=1:i
-    axes(ax4); % bring figure 4 axes to front
-    [PSx_late(j),PSy_late(j)] = ginput(1);
-    plot(PSx_late(j),PSy_late(j),'.','markersize',10,'color','y');
-    drawnow
+disp('Follow prompts to Zoom in & out on the DEMs & select icebergs for analysis.')
+disp('Try to focus on big icebergs. Max icebergs = 30!');
+
+%iterative iceberg selection
+q=1;
+while q
+    ID_question = questdlg('Are there matching icebergs?',...
+        'Iceberg Pairing','1) Yes!','2) No!','1) Yes!');
+    if q <= 30
+        %execute blunder removal based on question response
+        switch ID_question
+            case '1) Yes!'
+                disp(['iceberg #',num2str(q)]);
+                
+                %zoom in
+                disp('earlier DEM');
+                figure(figure1);
+                disp('Click on UL & LR corners of a box bounding a region where you want to look at icebergs to zoom in'); % Upper left, lower right.
+                [a] = ginput(2);
+                set(gca,'xlim',[min(a(:,1)) max(a(:,1))],'ylim',[min(a(:,2)) max(a(:,2))]);
+                drawnow; clear a;
+                disp('later DEM');
+                figure(figure2);
+                disp('Click on UL & LR corners of a box bounding a region where you want to look at icebergs to zoom in'); % Upper left, lower right.
+                [a] = ginput(2);
+                set(gca,'xlim',[min(a(:,1)) max(a(:,1))],'ylim',[min(a(:,2)) max(a(:,2))]);
+                drawnow; clear a;
+                
+                disp('Click on the middle of the iceberg in each DEM');
+                disp('earlier DEM');
+                figure(figure1);
+                [PSx_early(q),PSy_early(q)] = ginput(1);
+                plot(PSx_early(q),PSy_early(q),'.','markersize',10,'color','y');
+                drawnow;
+                disp('later DEM');
+                figure(figure2);
+                [PSx_late(q),PSy_late(q)] = ginput(1);
+                plot(PSx_late(q),PSy_late(q),'.','markersize',10,'color','y');
+                drawnow;
+                
+                %zoom back out & advance
+                q = q+1;
+                figure(figure1);
+                set(gca,'xlim',[min(DEM1.x) max(DEM1.x)],'ylim',[min(DEM1.y) max(DEM1.y)]);
+                figure(figure2);
+                set(gca,'xlim',[min(DEM2.x) max(DEM2.x)],'ylim',[min(DEM2.y) max(DEM2.y)]);
+                drawnow;
+            case '2) No!'
+                disp('Move on!')
+                break
+        end
+    else
+        disp('Matched 30 icebergs. Move on!');
+        break
+    end
+    clear ID_question;
 end
 
-% sort by x coordinates then y-coordinates
+% % col = spring(30); % color scheme for plotting
+% more=1; % select more icebergs when more=1
+% i=0; % index for storing coordinates
+% axes(ax3); % bring figure 3 axes to front
+% while more==1
+%     axes(ax3);
+%     i=i+1;
+%     [PSx_early(i),PSy_early(i)] = ginput(1);
+%     plot(gca,PSx_early(i),PSy_early(i),'.','markersize',10,'color','y');
+%     
+%     str = input('Do you see more icebergs (y/n)?','s');
+%     if strcmp(str,'n')
+%         more=more+1;
+%     end 
+% end
+% %add number labels
+% axes(ax3);
+% for i = 1:length(PSx_early)
+%    text(PSx_early(i)+300,PSy_early(i),num2str(i),'color','y','fontsize',18); hold on;
+% end
+% disp('later DEM: Now, click on the same icebergs in the other image');
+% disp('   You can click until the same number of icebergs have been selected');
+% figure(figure4);
+% for j=1:i
+%     axes(ax4); % bring figure 4 axes to front
+%     [PSx_late(j),PSy_late(j)] = ginput(1);
+%     plot(PSx_late(j),PSy_late(j),'.','markersize',10,'color','y');
+%     drawnow
+% end
+
+% concatenate coordinates
 early_coords = [PSx_early',PSy_early']; %early_coords = sortrows(early_coords,[1 2]);
 PSx_early = early_coords(:,1); PSy_early = early_coords(:,2);
 late_coords = [PSx_late',PSy_late']; %late_coords = sortrows(late_coords,[1 2]);
